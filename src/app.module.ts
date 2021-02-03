0a06d8c2f313af72f898f68cb9832bd7a2a97dde
Jimmi <mgammal@lucidya.com>
2021-02-03 17:00:02 +0200

    all in one

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { PostsModule } from './posts/posts.module';
+import { MessagesModule } from './messages/messages.module';
 import { ProductsController } from './products/products.controller';
 import { ProductsModule } from './products/products.module';
 import { ProductsService } from './products/products.service';
+import { UsersModule } from './users/users.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
   imports: [
     ProductsModule,
     PostsModule,
+    MessagesModule,
+    UsersModule,
+    AuthenticationModule,
     ConfigModule.forRoot({
       validationSchema: Joi.object({
         POSTGRES_HOST: Joi.string().required(),
@@ -21,6 +27,8 @@ import { ProductsService } from './products/products.service';
         POSTGRES_PASSWORD: Joi.string().required(),
         POSTGRES_DB: Joi.string().required(),
         PORT: Joi.number(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION_TIME: Joi.string().required()
       })
     }),
     DatabaseModule,
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as cookieParser from 'cookie-parser';
+import { join } from 'path';
+import { RedisIoAdapter } from './adapters/redis.adapter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useWebSocketAdapter(new RedisIoAdapter(app));
+  app.useStaticAssets(join(__dirname, '..', 'resource'));
+  app.use(cookieParser());
+  const port = parseInt(process.env.SERVER_PORT);
+  await app.listen(port);
 }
 bootstrap();
